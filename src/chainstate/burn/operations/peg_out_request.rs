b96c25c4ae692c2f2e20bc7e7417b527e39be3b9
Mårten Blankfors <marten@blankfors.se>
2023-01-17 16:12:00 +0100

    feat: Data parsing for peg-out request

--- src/chainstate/burn/operations/peg_out_request.rs
+++ src/chainstate/burn/operations/peg_out_request.rs
@@ -1,4 +1,5 @@
 use clarity::codec::StacksMessageCodec;
+use clarity::util::secp256k1::MessageSignature;
 
 use crate::burnchains::BurnchainBlockHeader;
 use crate::burnchains::BurnchainTransaction;
@@ -17,8 +18,33 @@ impl PegOutRequestOp {
         todo!();
     }
 
-    fn parse_data(data: &[u8]) -> Result<StacksAddress, ParseError> {
-        todo!();
+    fn parse_data(data: &[u8]) -> Result<ParsedData, ParseError> {
+        /*
+            Wire format:
+
+            0      2  3         11                76
+            |------|--|---------|-----------------|
+             magic  op   amount      signature
+
+             Note that `data` is missing the first 3 bytes -- the magic and op must
+             be stripped before this method is called. At the time of writing,
+             this is done in `burnchains::bitcoin::blocks::BitcoinBlockParser::parse_data`.
+        */
+
+        if data.len() < 73 {
+            // too short
+            warn!(
+                "PegOutRequestOp payload is malformed ({} bytes, expected {})",
+                data.len(),
+                73
+            );
+            return Err(ParseError::MalformedPayload);
+        }
+
+        let amount = u64::from_be_bytes(data[0..8].try_into()?);
+        let signature = MessageSignature(data[8..73].try_into()?);
+
+        Ok(ParsedData { amount, signature })
     }
 
     pub fn check(&self) -> Result<(), OpError> {
@@ -26,13 +52,25 @@ impl PegOutRequestOp {
     }
 }
 
+struct ParsedData {
+    amount: u64,
+    signature: MessageSignature,
+}
+
 enum ParseError {
-    AddressParsing,
+    MalformedPayload,
+    SliceConversion,
 }
 
 impl From<ParseError> for OpError {
     fn from(_: ParseError) -> Self {
         Self::ParseError
+    }
+}
+
+impl From<std::array::TryFromSliceError> for ParseError {
+    fn from(_: std::array::TryFromSliceError) -> Self {
+        Self::SliceConversion
     }
 }
 
